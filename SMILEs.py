67eed1cdaee797f4a7f459bf3fbd3982bf5e3e8c
Kristina <klilova165@gmail.com>
2022-01-13 22:50:20 +0200

    added, freckles,eyes,mouth etc

--- SMILEs.py
+++ SMILEs.py
@@ -1,20 +1,34 @@
 from tkinter import * 
-a=3
-b=0
-c=0
-d=0
+
 def Face():
-	global a,draw
-	if a==0:
-		draw.create_oval((5, 5, 290, 290))
-		a=1
-	elif a==1:
-		draw.create_rectangle((15, 15, 290, 290), width="2")
-	else:
-		pass
-		
+	draw.create_oval((5, 5, 290, 290),fill="pink",outline="black")
+
 def Nose():
-	draw.create_polygon([(150, 200), (150, 15), (15,150 )],fill="black")
+	draw.create_polygon([(150, 163), (181, 163), (150,130 )],fill="grey")
+
+def Mouth():
+	draw.create_line((213, 215, 90, 215),fill="black",width=3)
+
+def Eyes():
+	draw.create_oval((72,72,130,130),fill="white")
+	draw.create_oval((165,72,221,130),fill="white")
+	draw.create_oval((82,82,117,120),fill="black")
+	draw.create_oval((175,82,211,120),fill="black")
+
+def Freckles():
+	draw.create_line((198,145,200,145),fill="brown",width=2)
+	draw.create_line((227,169,230,169),fill="brown",width=2)
+	draw.create_line((130,127,133,127),fill="brown",width=2)
+	draw.create_line((147,173,150,173),fill="brown",width=2)
+	draw.create_line((146,150,150,150),fill="brown",width=2)
+	draw.create_line((80,134,82,134),fill="brown",width=2)
+	draw.create_line((174,181,177,181),fill="brown",width=2)
+	draw.create_line((155,91,158,91),fill="brown",width=2)
+	draw.create_line((119,150,122,150),fill="brown",width=2)
+	draw.create_line((108,145,111,145),fill="brown",width=2)
+	draw.create_line((241,129,244,129),fill="brown",width=2)
+	draw.create_line((111,193,113,193),fill="brown",width=2)
+
 tk=Tk()
 tk.title("Фоторобот")
 tk.geometry("700x300")
@@ -27,13 +41,14 @@ f2.pack(side=RIGHT)
 name=Label(f1,text="Создание фоторобота",font="Calibri 26",fg="black",bg="lightblue",justify=CENTER)
 name.pack(side=TOP)
 face=Checkbutton(f1,text="Лицо",font="Calibri 26",fg="black",bg="lightgreen",relief=GROOVE,command=Face)
-mouth=Checkbutton(f1,text="Рот",font="Calibri 26",fg="black",bg="lightgreen",relief=GROOVE)
+mouth=Checkbutton(f1,text="Рот",font="Calibri 26",fg="black",bg="lightgreen",relief=GROOVE,command=Mouth)
 nose=Checkbutton(f1,text="Нос",font="Calibri 26",fg="black",bg="lightgreen",relief=GROOVE,command=Nose)
-eyes=Checkbutton(f1,text="Глаза",font="Calibri 26",fg="black",bg="lightgreen",relief=GROOVE)
-
+eyes=Checkbutton(f1,text="Глаза",font="Calibri 26",fg="black",bg="lightgreen",relief=GROOVE,command=Eyes)
+freckles=Checkbutton(f1,text="веснушки",font="Calibri 26",fg="black",bg="lightgreen",relief=GROOVE,command=Freckles)
 face.pack()
 mouth.pack()
 nose.pack()
 eyes.pack()
+freckles.pack()
 
 tk.mainloop()
