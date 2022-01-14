6f89f3a34524b9c92a63cc04edc32feb083be985
opilane <opilane@HP-CZC6297BB1>
2022-01-14 11:24:04 +0200

    d-done..

--- SMILEs.py
+++ SMILEs.py
@@ -1,50 +1,86 @@
 from tkinter import * 
 
 def Face():
-	draw.create_oval((5, 5, 290, 290),fill="pink",outline="black")
+	global fac
+	if var_face.get()=="лицо":
+		fac=draw.create_oval((5, 5, 290, 290),fill="pink",outline="black")
+	else:
+		draw.delete(fac)
 
 def Nose():
-	draw.create_polygon([(150, 163), (181, 163), (150,130 )],fill="grey")
+	global eye
+	if var_nose.get()=="нос":
+		eye=draw.create_polygon([(150, 163), (181, 163), (150,130 )],fill="grey")
+	else:
+		draw.delete(eye)
 
 def Mouth():
-	draw.create_line((213, 215, 90, 215),fill="black",width=3)
+	global a
+	if var_mouth.get()=="рот":
+		a=draw.create_line((213, 215, 90, 215),fill="black",width=3)
+	else:
+		draw.delete(a)
 
 def Eyes():
-	draw.create_oval((72,72,130,130),fill="white")
-	draw.create_oval((165,72,221,130),fill="white")
-	draw.create_oval((82,82,117,120),fill="black")
-	draw.create_oval((175,82,211,120),fill="black")
+	global b,c,d,e
+	if var_eyes.get()=="глаза":
+		b=draw.create_oval((72,72,130,130),fill="white")
+		c=draw.create_oval((165,72,221,130),fill="white")
+		d=draw.create_oval((82,82,117,120),fill="black")
+		e=draw.create_oval((175,82,211,120),fill="black")
+	else:
+		draw.delete(b,c,d,e)
 
 def Freckles():
-	draw.create_line((198,145,200,145),fill="brown",width=2)
-	draw.create_line((227,169,230,169),fill="brown",width=2)
-	draw.create_line((130,127,133,127),fill="brown",width=2)
-	draw.create_line((147,173,150,173),fill="brown",width=2)
-	draw.create_line((146,150,150,150),fill="brown",width=2)
-	draw.create_line((80,134,82,134),fill="brown",width=2)
-	draw.create_line((174,181,177,181),fill="brown",width=2)
-	draw.create_line((155,91,158,91),fill="brown",width=2)
-	draw.create_line((119,150,122,150),fill="brown",width=2)
-	draw.create_line((108,145,111,145),fill="brown",width=2)
-	draw.create_line((241,129,244,129),fill="brown",width=2)
-	draw.create_line((111,193,113,193),fill="brown",width=2)
+	global a1,a2,a3,a4,a5,a6,a7,a8,a9,a10,a11,a12
+	if var_freckles.get()=="весн":
+		a1=draw.create_line((198,145,200,145),fill="brown",width=2)
+		a2=draw.create_line((227,169,230,169),fill="brown",width=2)
+		a3=draw.create_line((130,127,133,127),fill="brown",width=2)
+		a4=draw.create_line((147,173,150,173),fill="brown",width=2)
+		a5=draw.create_line((146,150,150,150),fill="brown",width=2)
+		a6=draw.create_line((80,134,82,134),fill="brown",width=2)
+		a7=draw.create_line((174,181,177,181),fill="brown",width=2)
+		a8=draw.create_line((155,91,158,91),fill="brown",width=2)
+		a9=draw.create_line((119,150,122,150),fill="brown",width=2)
+		a10=draw.create_line((108,145,111,145),fill="brown",width=2)
+		a11=draw.create_line((241,129,244,129),fill="brown",width=2)
+		a12=draw.create_line((111,193,113,193),fill="brown",width=2)
+	else:
+		draw.delete(a1,a2,a3,a4,a5,a6,a7,a8,a9,a10,a11,a12)
 
 tk=Tk()
 tk.title("Фоторобот")
-tk.geometry("700x300")
+tk.geometry("700x400")
 f1=Frame(tk,width=300,height=300)
-f2=Frame(tk,width=400,height=300)
-draw=Canvas(f2,width=300,height=300,bg="green")
+f2=Frame(tk,width=400,height=400)
+draw=Canvas(f2,width=300,height=300,bg="lightgreen")
 draw.pack()
 f1.pack(side=LEFT)
 f2.pack(side=RIGHT)
 name=Label(f1,text="Создание фоторобота",font="Calibri 26",fg="black",bg="lightblue",justify=CENTER)
 name.pack(side=TOP)
-face=Checkbutton(f1,text="Лицо",font="Calibri 26",fg="black",bg="lightgreen",relief=GROOVE,command=Face)
-mouth=Checkbutton(f1,text="Рот",font="Calibri 26",fg="black",bg="lightgreen",relief=GROOVE,command=Mouth)
-nose=Checkbutton(f1,text="Нос",font="Calibri 26",fg="black",bg="lightgreen",relief=GROOVE,command=Nose)
-eyes=Checkbutton(f1,text="Глаза",font="Calibri 26",fg="black",bg="lightgreen",relief=GROOVE,command=Eyes)
-freckles=Checkbutton(f1,text="веснушки",font="Calibri 26",fg="black",bg="lightgreen",relief=GROOVE,command=Freckles)
+
+var_face=StringVar()
+face=Checkbutton(f1,text="Лицо",font="Calibri 26",fg="black",bg="lightgreen",relief=GROOVE,variable=var_face,offvalue="убрать",onvalue="лицо",command=Face)
+face.deselect()
+
+var_mouth=StringVar()
+mouth=Checkbutton(f1,text="Рот",font="Calibri 26",fg="black",bg="lightgreen",relief=GROOVE,variable=var_mouth,onvalue="рот",offvalue="убрать",command=Mouth)
+mouth.deselect()
+
+var_nose=StringVar()
+nose=Checkbutton(f1,text="Нос",font="Calibri 26",fg="black",bg="lightgreen",relief=GROOVE,variable=var_nose,onvalue="нос",offvalue="убрать",command=Nose)
+nose.deselect()
+
+var_eyes=StringVar()
+eyes=Checkbutton(f1,text="Глаза",font="Calibri 26",fg="black",bg="lightgreen",relief=GROOVE,variable=var_eyes,onvalue="глаза",offvalue="убрать",command=Eyes)
+eyes.deselect()
+
+var_freckles=StringVar()
+freckles=Checkbutton(f1,text="веснушки",font="Calibri 26",fg="black",bg="lightgreen",relief=GROOVE,variable=var_freckles,onvalue="весн",offvalue="убрать",command=Freckles)
+freckles.deselect()
+
 face.pack()
 mouth.pack()
 nose.pack()
--- naidis.py
+++ naidis.py
@@ -4,30 +4,30 @@ from tkinter import *
 tk = Tk() 
 # Рисунок 300x300 пикселей, фон - белый 
 c = Canvas(tk, width=300, height=300, bg="white") 
-c.create_arc((5, 5, 50, 50), style=PIESLICE)
+#c.create_arc((5, 5, 50, 50), style=PIESLICE)
 # Сектор ("кусок пирога") 
-c.create_arc((55, 5, 100, 50), style=ARC)
+c.create_arc((150, 100, 200, 55), style=ARC)
 # Дуга 
-c.create_arc((105, 5, 150, 50), style=CHORD, start=0, extent=150, fill="blue") # от 0 до 150 градусов # Ломаная со стрелкой на конце 
-c.create_line([(5, 55), (55, 55), (30, 95)], arrow=LAST) 
+#c.create_arc((105, 5, 150, 50), style=CHORD, start=0, extent=150, fill="blue") # от 0 до 150 градусов # Ломаная со стрелкой на конце 
+#c.create_line([(5, 55), (55, 55), (30, 95)], arrow=LAST) 
 # Кривая (сглаженная ломаная) 
-c.create_line([(105, 55), (155, 55), (130, 95)], smooth=1) 
+#c.create_line([(105, 55), (155, 55), (130, 95)], smooth=1) 
 # Многоугольник зеленого цвета 
-c.create_polygon([(205, 55), (255, 55), (230, 95)], fill="green")
+#c.create_polygon([(205, 55), (255, 55), (230, 95)], fill="green")
 # Овал 
-c.create_oval((5, 105, 50, 120), ) 
+#c.create_oval((5, 105, 50, 120), ) 
 # Прямоугольник красного цвета с большой серой границей 
-c.create_rectangle((105, 105, 150, 130), fill="red", outline="grey", width="5") 
+#c.create_rectangle((105, 105, 150, 130), fill="red", outline="grey", width="5") 
 # Текст 
-c.create_text((5, 205), text=" Hello", anchor="nw") 
+#c.create_text((5, 205), text=" Hello", anchor="nw") 
 # Эта точка визуально обозначает угол привязки 
-c.create_oval((5, 205, 6, 206), outline="red") 
+#c.create_oval((5, 205, 6, 206), outline="red") 
 # Текст с заданным выравниванием 
-c.create_text((105, 205), text="Hello,\nmy friend!", justify=LEFT, anchor="c") 
-c.create_oval((105, 205, 106, 206), outline="red") 
+#c.create_text((105, 205), text="Hello,\nmy friend!", justify=LEFT, anchor="c") 
+#c.create_oval((105, 205, 106, 206), outline="red") 
 # Еще один вариант 
-c.create_text((205, 205), text="Hello,\nmy friend!", justify=CENTER, anchor="se") 
-c.create_oval((205, 205, 206, 206), outline="red") 
+#c.create_text((205, 205), text="Hello,\nmy friend!", justify=CENTER, anchor="se") 
+#c.create_oval((205, 205, 206, 206), outline="red") 
 c.pack() 
 tk.mainloop() 
 
